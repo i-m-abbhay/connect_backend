948b5e178256ef5d55871c76b570f2e69ee2e543
i-m-abbhay <20je0018@me.iitism.ac.in>
2023-01-29 00:45:10 +0530

    created-schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,3 +1,7 @@
 import user from './user'
+import pin from './pin'
+import save from './save'
+import comment from './comment'
+import postedBy from './postedBy'
 // Then we give our schema to the builder and provide the result to
-export const schemaTypes = [user]
+export const schemaTypes = [user, pin, comment, postedBy, save]
